56d0f6134b5efde754d41911edbb9d0a86099804
TeppalaSateesh <teppalasateesh@gmail.com>
2021-06-29 15:14:34 +0530

    vm

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,13 +131,17 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
+MEDIA_URL = '/images/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static')
-]
+)
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,'templates'),
+    )
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -28,3 +28,9 @@ urlpatterns = [
     path('dairy/',include('dairy.urls'))
 
 ]
+from django.conf import settings
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    )
